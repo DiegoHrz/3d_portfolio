a6c3ab9f641cbb97b62890f772ba3086791b7327
DiegoHrz <hernandezwdiego@gmail.com>
2023-07-16 01:10:11 -0500

    1er commit

---
+++ instrucciones.js
@@ -0,0 +1,10 @@
+
+
+/* npm create vite@latest ./ -- --template react to create a new react application
+//then install packages:
+//tailwind css: utility first css framework     =>    npm install -D tailwindcss then   =>    npx tailwindcss init
+// adittional packages: 
+// npm install --legacy-peer-deps @react-three/fiber @react-three/drei maath react-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom 
+
+
+*/
