9e5e689346ceae6fd05ba4ce69056442fbcfe8a7
DiegoHrz <hernandezwdiego@gmail.com>
2023-07-20 11:35:51 -0500

    Add About section

--- instrucciones.js
+++ instrucciones.js
@@ -2,7 +2,7 @@
 
 /* npm create vite@latest ./ -- --template react to create a new react application
 then install packages:
-tailwind css: utility first css framework     =>    npm install -D tailwindcss then   =>    npx tailwindcss init
+tailwind css: utility first css framework     =>    npm install -D tailwindcss postcss autoprefixed then   =>    npx tailwindcss init
 adittional packages: 
 npm install --legacy-peer-deps @react-three/fiber @react-three/drei maath react-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom 
     we use 'legacy peer deps for older react packages in this case for react-tilt.
@@ -16,6 +16,10 @@ npm install --legacy-peer-deps @react-three/fiber @react-three/drei maath react-
         react-router-dom       =>       for routing
 
 Then delete App.css
+
+
+
+
 
 
 
